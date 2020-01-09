bb032dab6b8bafe638b11f298eef8dca4a8d7401
Jeff Tian <jie.tian@unicareer.com>
2020-01-09 14:04:15 +0800

    feat: whitelist pa-ca.me

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
     cors: {
       origin: [
         /\.jiwai\.win$/,
+        /\.pa-ca\.me$/,
         /^http:\/\/localhost/,
-        /^https:\/\/uni\-sso\.herokuapp\.com/,
+        /^https:\/\/uni-sso\.herokuapp\.com/,
+        /^https:\/\/gatsby-sso\.netlify\.com/,
       ],
       methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
       preflightContinue: false,
