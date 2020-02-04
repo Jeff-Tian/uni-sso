1e7bb43ebe35b2d1913a7c70ff6581225a15cd2f
Jeff Tian <jeff.tian@outlook.com>
2020-02-04 12:55:54 +0800

    fix: tests

--- src/config/config.service.spec.ts
+++ src/config/config.service.spec.ts
@@ -2,6 +2,8 @@ import { ConfigService } from './config.service';
 
 describe('config', () => {
     it('default config', () => {
+        delete process.env.MONGODB_URI;
+
         const configService = new ConfigService('/not/exists');
         expect(configService.get('MONGODB_URI')).toEqual('mongodb://host.docker.internal:27017');
         expect(configService.get('PORT')).toEqual(3000);
