6835d29ee669cf29cd8d27e2706141fe8836e31c
Jeff Tian <jeff.tian@outlook.com>
2020-02-04 12:05:23 +0800

    test: test read from environment

--- src/config/config.service.spec.ts
+++ src/config/config.service.spec.ts
@@ -6,4 +6,11 @@ describe('config', () => {
         expect(configService.get('MONGODB_URI')).toEqual('mongodb://host.docker.internal:27017');
         expect(configService.get('PORT')).toEqual(3000);
     });
+
+    it('read from environment', () => {
+        process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017';
+
+        const configService = new ConfigService('/not/exists');
+        expect(configService.get('MONGODB_URI')).toEqual('mongodb://127.0.0.1:27017');
+    });
 });
