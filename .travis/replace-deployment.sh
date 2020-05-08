6deeef7ac4edd3826cb2986ac14eef0047e79a46
Jeff Tian <Jeff.Tian@externals.adidas.com>
2020-05-08 19:23:06 +0800

    ci: trigger deployment image change

---
+++ .travis/replace-deployment.sh
@@ -0,0 +1,3 @@
+sed -i .bak "s/\'jefftian\/uni-sso:latest\'/\'jefftian\/uni-sso\'/g" inf/prod/deployment.yaml
+
+# sed -i .bak "s/\'jefftian\/uni-sso\'/\'jefftian\/uni-sso:latest\'/g" inf/prod/deployment.yaml
