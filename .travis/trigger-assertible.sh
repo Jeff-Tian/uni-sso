06c8910493153b0f5bbb8600c89c5865adb80963
Jeff Tian <Jeff.Tian@externals.adidas.com>
2020-05-29 10:16:52 +0800

    fix: make assertible trigger to dedicated shell file

---
+++ .travis/trigger-assertible.sh
@@ -0,0 +1,4 @@
+curl -u IlKnoRQ5yY3mYd1R: "https://assertible.com/deployments" -d'{
+                                "service": "ed5330e3-148c-41c1-b76c-c5d6046227a0",
+                                "environment": "production",
+                                "version": "v1"}'
