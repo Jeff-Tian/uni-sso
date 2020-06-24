65cd849c29e0d4876c529f494f886ed73f9589ba
Jeff Tian <Jeff.Tian@outlook.com>
2020-06-24 12:32:07 +0800

    feat: use fastify

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,28 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import xmlParser from 'body-parser-xml';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 xmlParser(bodyParser);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: trustedHosts,
-      methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+    {
+      cors: {
+        origin: trustedHosts,
+        methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+      },
     },
-  });
+  );
   app.use(cookieParser());
   app.use(
     // @ts-ignore
@@ -40,7 +50,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(Number(process.env.PORT) || 3000, '0.0.0.0');
 }
 
 bootstrap();
